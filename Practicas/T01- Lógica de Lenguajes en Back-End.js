c16ebe7c47d12c7ec77ae23d2b517b87dfb61ccd
MorenoBello1 <84943053+MorenoBello1@users.noreply.github.com>
2022-09-29 16:31:26 -0500

    Add files via upload

---
+++ Practicas/T01- Lógica de Lenguajes en Back-End.js
@@ -0,0 +1,50 @@
+//Creaciones de los array para los respectivos Objetos:Plato, Cliente, Registro.
+var Plato =[
+
+    {  idPlato:1, Nombre_de_Plato:"Crema de verdura"},      
+    {  idPlato:2, Nombre_de_Plato:"Crema de Queso"},
+    {  idPlato:3, Nombre_de_Plato:"Camaron Apanado" },
+    {  idPlato:4, Nombre_de_Plato:"Arroz blanco"},
+    {  idPlato:5, Nombre_de_Plato:"Frutas"}
+];
+
+//Se añadirieron 2 atributos que se creen necesarios "Edad y  Altura" 
+var Cliente =[
+
+    {   idCliente:1, Nombre:"Jose", Identificacion:"1314845791",Edad:21, Altura:1.78},      
+    {   idCliente:2, Nombre:"Andres", Identificacion:"1312745721",Edad:21, Altura:1.82},
+    {   idCliente:3, Nombre:"Juliana", Identificacion:"1315845721",Edad:21, Altura:1.81},
+    {   idCliente:4, Nombre:"Majo", Identificacion:"1312835211",Edad:21, Altura:1.78},
+    {   idCliente:5, Nombre:"Carla", Identificacion:"1315841741",Edad:21, Altura:1.63}
+];
+
+const Fecha_Hoy= new Date()
+
+var Registro =[
+//incorporamos las relaciones de Id.
+    {   idRegistro:1, idCliente:1, idPlato:1, fecha:Fecha_Hoy, hora:"12:33", NumeroCaloriasConsumidas:"20", NumeroPorciones:"3"},      
+    {   idRegistro:2, idCliente:2, idPlato:2, fecha:"22-08-2002", hora:"22:33", NumeroCaloriasConsumidas:"28", NumeroPorciones:"3"},
+    {   idRegistro:3, idCliente:3, idPlato:3, fecha:"12-04-2002", hora:"08:12", NumeroCaloriasConsumidas:"12", NumeroPorciones:"3"},
+    {   idRegistro:4, idCliente:4, idPlato:4, fecha:Fecha_Hoy, hora:"21:19", NumeroCaloriasConsumidas:"32", NumeroPorciones:"3"},
+    {   idRegistro:5, idCliente:5, idPlato:5, fecha:"22/08/2005", hora:"2:01", NumeroCaloriasConsumidas:"2", NumeroPorciones:"3"}
+];
+
+// Y utilizacion de .length para la distancia de los array de registro y Plato.
+let conteo=0;
+while (conteo<Plato.length){
+
+    conteo++;
+    console.log(Plato[conteo]);
+}
+
+//bucle para mostrar los datos del registro mediante una variable "i" que sera su aumento
+for (let i=0; i<Registro.length;i++){
+    console.log(Registro[i]);
+}
+
+//Uso de un forEach que apartir de el elemento "AtributoActual" hacemos el llamado de cada uno de las propiedades dentro del array cliente."
+Cliente.forEach(AtributoActual => console.log('\nId del Cliente: ' +AtributoActual.idCliente +'   -Nombre del Cliente: '+AtributoActual.Nombre
++'   Numero de identificacion: '+AtributoActual.Identificacion+ '    Con una edad de: '+AtributoActual.Edad+'    Y Altura de :  '+AtributoActual.Altura));
+
+ 
+  
