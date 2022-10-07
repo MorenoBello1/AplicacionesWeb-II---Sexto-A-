54c8fddb23db69c1919bb171624a8ef33de16b4f
MorenoBello1 <84943053+MorenoBello1@users.noreply.github.com>
2022-10-07 18:02:12 -0500

    Add files via upload

--- Practicas/T02- Datos.js
+++ Practicas/T02- Datos.js
@@ -13,7 +13,6 @@ var Registro =[
         {   id_Registro:2, id_Plato:2, fecha:"22-08-2002", hora:"22:33", Numero_Calorias_Consumidas:23, Numero_Porciones:3},
         {   id_Registro:3, id_Plato:3, fecha:"12-04-2002", hora:"08:12", Numero_Calorias_Consumidas:12, Numero_Porciones:1},
         {   id_Registro:4, id_Plato:4, fecha:"24-12-2022", hora:"21:19", Numero_Calorias_Consumidas:32, Numero_Porciones:30},
-
         {   id_Registro:5, id_Plato:5, fecha:"12-08-1999", hora:"2:01", Numero_Calorias_Consumidas:2, Numero_Porciones:2},
         {   id_Registro:6, id_Plato:1, fecha:"12-08-1999", hora:"2:01", Numero_Calorias_Consumidas:2, Numero_Porciones:2}
 
--- Practicas/T02- Funcion.js
+++ Practicas/T02- Funcion.js
@@ -1,23 +1,33 @@
 const error = new Error();
-const {Plato, Registro} = require ('./Datos');
-async function Busqueda(id_Regis){
-    const registros = Registro.find((registros)=> registros.id_Registro===id_Regis );
-    if (!registros)
+const {Plato, Registro} = require ('./T02- Datos');
+// Constantes, Una para generar error, y la otra para extrar los array "Plato" y "Registro" de Datos.js
+
+async function Busqueda(aux_id_Registro){
+    const registros = Registro.find((registros)=> registros.id_Registro===aux_id_Registro );
+    //Constante de registro para conocer si existe el registro que se envia de aux_Id_Registro del array Registro.
+    if (!registros) 
     {
         error.message="No encontramos el registro";
-        throw error;
+        throw error;//si  no se encuentre el aux_id_Registro lanzara el error.
     }
-    
     else{
-        Rela_Plato=Plato.find(item=> item.id_Plato===registros.id_Plato );
-        console.log(`Id de Registro: ${registros.id_Registro} ,Nombre del plato: ${Rela_Plato.Nombre_de_Plato} Porciones ${registros.Numero_Porciones}  Sus Calorias: ${registros.Numero_Calorias_Consumidas}`);
+        //Caso contrario, se crea una relacion de el id_plato que debe tener la  entidad transaccional 
+        Relacion_Plato=Plato.find(item=> item.id_Plato===registros.id_Plato );
+        console.log(`Id de Registro: ${registros.id_Registro} ,Nombre del plato: ${Relacion_Plato.Nombre_de_Plato} Porciones ${registros.Numero_Porciones}  Sus Calorias: ${registros.Numero_Calorias_Consumidas}`);
         console.log(`Con fecha de: ${registros.fecha}  -  ${registros.hora} \n`);
     }  
 }
 (async ()=>{
     try
     {
-        await Busqueda(7);
+        //en este caso enviamos "5" metodo de busqueda como Aux_id_Registro
+        await Busqueda(5, find_Plato(5,(err,Platos)=>{
+        //Ademas ejecutamos una funcion con un callback en este caso enviamos "5" si existe el id_plato continua si no lanza error.
+            if(err) return console.log(err);
+            console.log(Platos);
+        }));
+         
+        
     }
     catch (err)
     {
@@ -25,3 +35,14 @@ async function Busqueda(id_Regis){
     }
 }
 )();
+//funcion  de Busqueda de plato mediante el callBack.
+function find_Plato (aux_Nombre_de_Plato,CallBack){
+    const Platos = Plato.find(Platos=> Platos.id_Plato===aux_Nombre_de_Plato );
+    if (!Platos) {
+
+        CallBack(`\nNo se encontro el Plato con id : ${aux_Nombre_de_Plato}.`);
+    }else {
+        CallBack(null,`\nSe ha encontrado el Plato con la Id : ${Platos.id_Plato} ,pertenece a :  ${Platos.Nombre_de_Plato}.`)
+
+    }
+}
